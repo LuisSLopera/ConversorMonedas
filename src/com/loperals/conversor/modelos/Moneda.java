c4010c9600a16b2b13da1a7e1a333e39c4c32bd0
Luis Lopera <sistemas@zencillo.com>
2025-11-11 19:07:11 -0500

    Inicio definicion objeto moneda

---
+++ src/com/loperals/conversor/modelos/Moneda.java
@@ -0,0 +1,37 @@
+package com.loperals.conversor.modelos;
+
+public class Moneda {
+    String monedaOrigen;
+    String monedaDestino;
+    double valor;
+
+    public Moneda(String monedaOrigen, String monedaDestino, double valor) {
+        this.monedaOrigen = monedaOrigen;
+        this.monedaDestino = monedaDestino;
+        this.valor = valor;
+    }
+
+    public String getMonedaOrigen() {
+        return monedaOrigen;
+    }
+
+    public void setMonedaOrigen(String monedaOrigen) {
+        this.monedaOrigen = monedaOrigen;
+    }
+
+    public String getMonedaDestino() {
+        return monedaDestino;
+    }
+
+    public void setMonedaDestino(String monedaDestino) {
+        this.monedaDestino = monedaDestino;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
